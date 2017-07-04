dbbb1cf8e9c519cbb8bcb06cb88b640345f56e2b
Your Name <you@example.com>
2017-07-04 15:46:47 +0800

    add

---
+++ demo-parent/src/main/java/com/example/demo/DemoParentApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParentApplication.class, args);
+	}
+}
---
+++ demo-parent/src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.domain;
+
+import java.util.Date;
+
+public class User {
+	private int id;
+	private String name;
+	private Date date;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public User(int id, String name, Date date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.date = date;
+	}
+}
